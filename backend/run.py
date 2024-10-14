c70b393e34299964b1fb260da3dc5cc6cd39b94e
theVitalS <vitalii.sharovskyi@gmail.com>
2024-10-15 00:03:27 +0200

    Cleaned run file

--- backend/run.py
+++ backend/run.py
@@ -2,19 +2,5 @@ from app.app import db, app
 from database.sqlite_setup import *
 from database.models import *
 
-#rewrite_tables_from_models()
+create_empty_database()
 
-db_uri = 'sqlite:////home/vital/PycharmProjects/Lernotik/database/database.db'
-
-# Create the engine
-engine = create_engine(db_uri)
-
-Session = sessionmaker(bind=engine)
-session = Session()
-#session.close()
-
-subj = Subject(name='Subject1')
-session.add(subj)
-session.commit()
-
-session.close()
